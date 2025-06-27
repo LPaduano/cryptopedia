d45bfb7b54798e2746ab9d402e67f1b6dd468ad2
Luca Paduano <l.paduano@reply.it>
2025-06-27 10:52:57 +0200

    wip news subsection

---
+++ app/components/NewsDisplay.tsx
@@ -0,0 +1,66 @@
+// components/NewsDisplay.tsx
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+interface NewsItem {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+}
+
+interface NewsDisplayProps {
+  news: NewsItem[];
+}
+
+const NewsDisplay = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchAllCryptoNews = async () => {
+    try {
+      const res = await fetch("https://cryptopedia-ai-new.vercel.app/api/news");
+      if (!res.ok) throw new Error("Errore nel recupero dei dati dal DB");
+
+      const data = await res.json();
+      setNews(data);
+    } catch (error) {
+      console.error("Errore nel recupero delle crypto:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchAllCryptoNews();
+  }, []);
+  return (
+    <section className="mt-10 px-4">
+      <h2 className="text-2xl font-bold mb-4">Crypto News</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        {news.map((item: any, idx) => (
+          <a
+            key={idx}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative block h-64 overflow-hidden rounded-lg shadow-lg group"
+          >
+            <img
+              src={item.image}
+              alt={item.title}
+              className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-105"
+            />
+            <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black via-transparent to-transparent p-4">
+              <h3 className="text-white font-semibold text-lg">{item.title}</h3>
+              <p className="text-gray-200 text-sm mt-1 line-clamp-3">
+                {item.description}
+              </p>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default NewsDisplay;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import CryptoPrices from "./components/CryptoPrices";
+import NewsDisplay from "./components/NewsDisplay";
 
 export default function Home() {
   return (
     <div className="mt-12 pt-12 p-3">
       <div>
         <CryptoPrices />
+        <NewsDisplay />
       </div>
     </div>
   );
