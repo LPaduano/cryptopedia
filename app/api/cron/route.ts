e57a4fa301f15c875566e8a1a3517a919ed71d90
Luca Paduano <l.paduano@reply.it>
2025-06-28 21:43:08 +0200

    commented cron route

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import { saveCryptoToDb } from "../../../lib/saveCryptoToDb";
+// import { NextResponse } from "next/server";
+// import { saveCryptoToDb } from "../../../lib/saveCryptoToDb";
 
-export async function GET(req: Request) {
-  const auth = req.headers.get("Authorization");
+// export async function GET(req: Request) {
+//   const auth = req.headers.get("Authorization");
 
-  if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new NextResponse("Unauthorized", { status: 401 });
-  }
+//   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return new NextResponse("Unauthorized", { status: 401 });
+//   }
 
-  try {
-    await saveCryptoToDb();
-    return NextResponse.json({ ok: true });
-  } catch (err) {
-    console.error("Errore cron job:", err);
-    return new NextResponse("Errore interno", { status: 500 });
-  }
-}
+//   try {
+//     await saveCryptoToDb();
+//     return NextResponse.json({ ok: true });
+//   } catch (err) {
+//     console.error("Errore cron job:", err);
+//     return new NextResponse("Errore interno", { status: 500 });
+//   }
+// }
